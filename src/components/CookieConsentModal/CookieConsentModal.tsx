b3f1a3e12445873e512a1d83a9987393d290a598
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2025-09-09 22:38:13 -0300

    feat: add a cookie modal

---
+++ src/components/CookieConsentModal/CookieConsentModal.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import Modal from "../Modal/Modal"
+import Button from "../Button/Button";
+import styles from "./styles.module.scss";
+
+const COOKIE_CONSENT_KEY = "cookie_consent_given";
+
+const CookieConsentModal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    // A verificação do localStorage só pode acontecer no lado do cliente.
+    const consentGiven = localStorage.getItem(COOKIE_CONSENT_KEY);
+    if (!consentGiven) {
+      setIsOpen(true);
+    }
+  }, []);
+
+  const handleAction = (consent: "true" | "false") => {
+    localStorage.setItem(COOKIE_CONSENT_KEY, consent);
+    setIsOpen(false);
+  };
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={() => {}} // Passamos uma função vazia pois o fechamento é controlado pelos botões
+      disableBackdropClick={true}
+      disableEscapeKeyDown={true}
+    >
+      <Modal.Header hideCloseButton={true}>Valorizamos sua privacidade</Modal.Header>
+      <Modal.Body>
+        <p>
+          Nosso site usa cookies para melhorar a navegação. Sem eles você ainda poderá navegar, mas algumas funcionalidades não estarão disponíveis. Leia nossa Política de privacidade.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <div className={styles.cookieFooter}>
+          <Button
+            onClick={() => handleAction("false")}
+            label="Cancelar"
+            className={styles.confirmButton}
+          />
+          <Button className={styles.cancelButton} onClick={() => handleAction("true")} label="Aceitar" />
+        </div>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default CookieConsentModal;
---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,116 @@
+ "use client";
+
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  ReactNode,
+  useCallback,
+} from "react";
+import { X } from "lucide-react";
+import styles from "./styles.module.scss";
+
+type ModalContextType = {
+  onClose: () => void;
+};
+
+const ModalContext = createContext<ModalContextType | undefined>(undefined);
+
+type ModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+  disableBackdropClick?: boolean;
+  disableEscapeKeyDown?: boolean;
+};
+
+const useModalContext = () => {
+  const context = useContext(ModalContext);
+  if (!context) {
+    throw new Error("useModalContext must be used within a Modal");
+  }
+  return context;
+};
+
+const Modal = ({
+  isOpen,
+  onClose,
+  children,
+  disableBackdropClick = false,
+  disableEscapeKeyDown = false,
+}: ModalProps) => {
+  useEffect(() => {
+    const handleEsc = (event: KeyboardEvent) => {
+      if (!disableEscapeKeyDown && event.key === "Escape") {
+        onClose();
+      }
+    };
+    window.addEventListener("keydown", handleEsc);
+
+    return () => {
+      window.removeEventListener("keydown", handleEsc);
+    };
+  }, [onClose, disableEscapeKeyDown]);
+
+  const handleBackdropClick = useCallback(() => {
+    if (!disableBackdropClick) {
+      onClose();
+    }
+  }, [disableBackdropClick, onClose]);
+
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <ModalContext.Provider value={{ onClose }}>
+      <div className={styles.modalBackdrop} onClick={handleBackdropClick}>
+        <div
+          className={styles.modalContainer}
+          onClick={(e) => e.stopPropagation()}
+        >
+          {children}
+        </div>
+      </div>
+    </ModalContext.Provider>
+  );
+};
+
+const Header = ({
+  children,
+  hideCloseButton = false,
+}: {
+  children: ReactNode;
+  hideCloseButton?: boolean;
+}) => {
+  return (
+    <div className={styles.modalHeader}>
+      <h2 className={styles.modalTitle}>{children}</h2>
+      {!hideCloseButton && <CloseButton />}
+    </div>
+  );
+};
+
+const Body = ({ children }: { children: ReactNode }) => {
+  return <div className={styles.modalBody}>{children}</div>;
+};
+
+const Footer = ({ children }: { children: ReactNode }) => {
+  return <div className={styles.modalFooter}>{children}</div>;
+};
+
+const CloseButton = () => {
+  const { onClose } = useModalContext();
+  return (
+    <button className={styles.closeButton} onClick={onClose}>
+      <X size={24} />
+    </button>
+  );
+};
+
+Modal.Header = Header;
+Modal.Body = Body;
+Modal.Footer = Footer;
+Modal.CloseButton = CloseButton;
+
+export default Modal;
